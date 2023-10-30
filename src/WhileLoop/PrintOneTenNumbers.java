82aae966c533e046d7876f801243036b339929f5
skwasim777 <wasimsk6061@gmail.com>
2023-10-30 09:35:04 +0530

    printing one to ten numbers using while loop

---
+++ src/WhileLoop/PrintOneTenNumbers.java
@@ -0,0 +1,11 @@
+package WhileLoop;
+
+public class PrintOneTenNumbers {
+	public static void main(String[] args) {
+		int n = 1;
+		while (n <= 10) {
+			System.out.println(n);
+			n++;
+		}
+	}
+}
