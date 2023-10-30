46070d837a7e6bdc0958b23496820e1cc90df192
skwasim777 <wasimsk6061@gmail.com>
2023-10-30 11:13:03 +0530

    swaping two numbers in simple way

---
+++ src/WhileLoop/SwapTwoNumbers.java
@@ -0,0 +1,10 @@
+package WhileLoop;
+
+public class SwapTwoNumbers {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.printf("Before Swap a = %d , b = %d \n", a, b);
+		System.out.printf("After Swap a = %d ,  b = %d ", b, a);
+	}
+}
